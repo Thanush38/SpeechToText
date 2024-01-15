1b2a7b9179644e008aaa243cea014dfa8b4f72c1
thanush <thanush38@outlook.com>
2024-01-15 12:33:28 -0500

    added listening feature

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace SpeechToText
 {
     public partial class Form1 : Form
     {
+        //set the recognizer and grammar
         SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
         Grammar grammer = new DictationGrammar();
         Boolean flag = false;
@@ -22,15 +23,13 @@ namespace SpeechToText
             InitializeComponent();
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
+        {   
+            //set the recognizer and grammar
             recognizer.SetInputToDefaultAudioDevice();
             recognizer.LoadGrammar(grammer);
+            //listen to the speech while flag is on
             while(flag)
             {
                 recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
@@ -46,6 +45,7 @@ namespace SpeechToText
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //sets the flag to true or false and changes the button text
             if(flag==false)
             {
                 flag = true;
@@ -64,6 +64,7 @@ namespace SpeechToText
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //speak the text in the richtextbox
             var synth = new SpeechSynthesizer();
             synth.SetOutputToDefaultAudioDevice();
             synth.SpeakAsync(richTextBox1.Text);
